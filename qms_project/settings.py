960d28d2e659642c67aa7c0aea8db79e2b0318ed
jijiknair <166284422+jijiknair@users.noreply.github.com>
2025-12-24 11:25:23 +0400

    Update settings.py

--- qms_project/settings.py
+++ qms_project/settings.py
@@ -79,9 +79,8 @@ WSGI_APPLICATION = 'qms_project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=True
+        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600
     )
 }
 
